ebdb12cd588907e73f695b809bae8a8f6983bc4c
bradley <111099770+goodtoseeyouz@users.noreply.github.com>
2025-09-28 10:59:49 -0700

    musician placement
    now are correct

--- main.py
+++ main.py
@@ -183,7 +183,10 @@ def spawn_musicians(radius, height, count):
         Musician(x=x, z=z, y=center.y)
 
 # Spawn 5 enemies in a smaller semicircle and 10 in a larger semicircle
-spawn_musicians(9, -6.5, 6)
+spawn_musicians(9, -6.5, 8)
+spawn_musicians(12, -6.5, 9)
+spawn_musicians(15, -6, 12)
+spawn_musicians(18, -6, 13)
 
 def pause_input(key):
     if key == 'tab':    # press tab to toggle edit/play mode
